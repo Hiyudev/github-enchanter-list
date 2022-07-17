5769c59ff9c4b1fcb66d37c87aeba4a85af7dbc5
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-17 09:03:18 -0300

    refactor: eslint formatting

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,6 +1,6 @@
 import constrast from 'contrast';
 
 export function getConstratColor(hex: string): string {
-    const res = constrast(hex);
-    return res === 'light' ? 'black' : 'white';
+  const res = constrast(hex);
+  return res === 'light' ? 'black' : 'white';
 }
