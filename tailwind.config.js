009959e4a009f1f5b92976e7e43bb9567db98053
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-17 08:54:38 -0300

    style: add default colors & styles for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
+  content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.pink,
+        secondary: colors.purple,
+        background: colors.zinc,
+      },
+    },
   },
   plugins: [],
 };
