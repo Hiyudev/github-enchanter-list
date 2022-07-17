26799b430cc284e1eb92cbb001d8fe1e5dbfb6b7
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-17 10:13:02 -0300

    feat: add theme support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider enableSystem attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
