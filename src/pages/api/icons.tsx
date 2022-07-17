dd4427bc4af94e418c405d0756f0446ee099c983
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-17 20:25:33 -0300

    feat(api): add api route to fetch all icons data

---
+++ src/pages/api/icons.tsx
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getIcons } from '../../lib/simpleIcons';
+import { SimpleIcon } from '../../types';
+
+type Data = {
+  icons: SimpleIcon[];
+};
+
+type ErrorResponse = {
+  message: string;
+};
+
+const getIconsByPageHandler = (
+  req: NextApiRequest,
+  res: NextApiResponse<Data | ErrorResponse>
+) => {
+  const { query } = req;
+  const { page, limit } = query;
+
+  const icons = getIcons();
+  const list: SimpleIcon[][] = [];
+
+  try {
+    const queryPage = Number(page);
+    const queryLimit = Number(limit);
+    if (!isNaN(queryPage) && !isNaN(queryLimit)) {
+      while (icons.length > 0) {
+        const chunk = icons.splice(0, queryLimit);
+        list.push(chunk);
+      }
+
+      const requestedPage = list[queryPage];
+
+      res.status(200).json({ icons: requestedPage });
+    }
+
+    throw new Error('Page and limit must be numbers');
+  } catch (err) {
+    return res.status(403).json({ message: err.message });
+  }
+};
+
+export default getIconsByPageHandler;
