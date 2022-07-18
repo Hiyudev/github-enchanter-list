3c8b030cba5b2823f60c1a05fab71518fc4c10fe
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-18 13:37:12 -0300

    feat: add infinite page scroll

--- src/components/sections/Banner/index.tsx
+++ src/components/sections/Banner/index.tsx
@@ -1,17 +1,33 @@
-import { SimpleIcon } from '../../../types';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import useSWRInfinite from 'swr/infinite';
 import BannerCard from './BannerCard';
 
-interface IBannerListProps {
-  icons: SimpleIcon[];
-}
+function BannerList() {
+  const itemsQuantity = 36;
 
-function BannerList({ icons }: IBannerListProps) {
+  const getIcons = (pageIndex, previousPageData) => {
+    if (previousPageData && !previousPageData.length) return null;
+    return `/api/icons?page=${pageIndex}&limit=${itemsQuantity}`;
+  };
+
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+
+  const { data, size, setSize } = useSWRInfinite(getIcons, fetcher);
+  if (!data) return <div>Loading...</div>;
+
   return (
-    <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {icons.slice(0, 10).map((icon, index) => {
-        return <BannerCard {...icon} key={index} />;
-      })}
-    </ul>
+    <InfiniteScroll
+      dataLength={data.length}
+      next={() => setSize(size + itemsQuantity)}
+      hasMore={true}
+      loader={<div>Loading</div>}
+    >
+      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {data.map((icons, index) => {
+          return icons.map((icon) => <BannerCard {...icon} key={icon.slug} />);
+        })}
+      </ul>
+    </InfiniteScroll>
   );
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,17 @@
-import { GetStaticProps, NextPage } from 'next/types';
+import { NextPage } from 'next/types';
 import Navbar from '../components/common/Navbar';
 import Layout from '../components/layout';
 import BannerList from '../components/sections/Banner';
-import { getIcons } from '../lib/simpleIcons';
-import { SimpleIcon } from '../types';
 
-interface IHomePageProps {
-  icons: SimpleIcon[];
-}
-
-const HomePage: NextPage = ({ icons }: IHomePageProps) => {
+const HomePage: NextPage = () => {
   return (
     <>
       <Navbar />
       <Layout className="mt-20">
-        <BannerList icons={icons} />
+        <BannerList />
       </Layout>
     </>
   );
 };
 
 export default HomePage;
-
-export const getStaticProps: GetStaticProps = async (context) => {
-  const icons = getIcons();
-
-  return {
-    props: {
-      icons,
-    },
-  };
-};
