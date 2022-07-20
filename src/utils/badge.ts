feaa2512e0fa6c772940a5d9641c807d9f9a5207
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-20 15:06:13 -0300

    feat!: removed badge url builder

--- src/utils/badge.ts
+++
@@ -1,9 +0,0 @@
-import { getConstratColor } from "../lib/color";
-
-export function generateBadgeURL(title: string, hex: string, slug: string): string {
-  const logoColor = getConstratColor(hex);
-  const encodedTitle = encodeURI(title).replaceAll("-", "_");
-
-  const url = `https://img.shields.io/badge/${encodedTitle}-${hex}?style=for-the-badge&logo=${slug}&logoColor=${logoColor}`;
-  return url;
-}
