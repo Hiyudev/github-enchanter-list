b43e488898f2ccddeae8ada1a47b942c0a701706
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-20 17:31:58 -0300

    feat!(api): change all logic of icons to badges

--- src/pages/api/badges.ts
+++ src/pages/api/badges.ts
@@ -1,9 +1,13 @@
 import fuzzysort from 'fuzzysort';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { SimpleIcon } from 'simple-icons';
+import { Badge } from '../../@types';
+import { getConstratColor } from '../../lib/color';
 import { getIcons } from '../../lib/simpleIcons';
-import { SimpleIcon } from '../../@types';
+import { urlEncode } from '../../utils/string';
+import { urls } from '../../utils/url';
 
-type Data = SimpleIcon[];
+type Data = Badge[];
 
 type ErrorResponse = {
   message: string;
@@ -13,35 +17,53 @@ const getBadgesHandler = (
   req: NextApiRequest,
   res: NextApiResponse<Data | ErrorResponse>
 ) => {
-  const { page, limit, search } = req.query;
+  const { page, limit, search, label, style } = req.query;
 
-  const icons = getIcons();
-  const list: SimpleIcon[][] = [];
+  const allIcons = getIcons();
+  const badges: Badge[] = [];
 
   try {
     const queryPage = Number(page);
     const queryLimit = Number(limit);
-    const querySearch = Array.isArray(search) ? search.join(" ") : search;
 
     if (!isNaN(queryPage) && !isNaN(queryLimit)) {
-      let iconsList: SimpleIcon[] = icons;
+      let iconsList: SimpleIcon[] = allIcons;
 
-      if (querySearch) {
-        iconsList = fuzzysort.go(querySearch, icons, { keys: ['title', 'slug'] }).map((icon) => icon.obj);
+      if (search) {
+        iconsList = fuzzysort.go(search as string, allIcons, { keys: ['title', 'slug'] }).map((result) => result.obj);
       }
 
-      while (iconsList.length > 0) {
-        const chunk = iconsList.splice(0, queryLimit);
-        list.push(chunk);
-      }
+      const start = queryPage * queryLimit;
+      const end = start + queryLimit;
+      const requestedIcons = iconsList.slice(start, end);
 
-      const requestedPage = list[queryPage];
+      requestedIcons.forEach((icon) => {
+        const logoColor = getConstratColor(icon.hex);
+        const labelString = label ? `${urlEncode(label as string)}-` : "";
+        const titleString = urlEncode(icon.title);
 
-      return res.status(200).json(requestedPage);
+        const hasStyle = typeof style === "string" && style.length > 0;
+
+        const templateUrl = `https://img.shields.io/badge/${labelString}${titleString}-${icon.hex}`
+
+        const url: string = urls(templateUrl, {
+          [`style=${style}`]: hasStyle,
+          [`logo=${icon.slug}`]: true,
+          [`logoColor=${logoColor}`]: true,
+        });
+
+        badges.push({
+          url,
+          name: icon.title,
+        });
+      });
+
+      return res.status(200).json(badges);
     }
 
     throw new Error('Page and limit must be numbers');
   } catch (err) {
+    console.log(err.message);
     return res.status(403).json({ message: err.message });
   }
 };
