e4388a844bd5f797f5a96d32d8c1dce43335758d
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-21 13:13:02 -0300

    feat: add home redirection and a11y support

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,12 +1,15 @@
+import Link from 'next/link';
 import LogoIcon from '../../icons/Logo';
 import ThemeSwitcher from './ThemeSwitcher';
 
 function Navbar() {
   return (
-    <nav className="bg-primary fixed top-0 z-50 flex w-full flex-row items-center justify-between p-5 sm:px-10 md:px-12 lg:px-24 xl:px-48">
-      <div>
-        <LogoIcon size={32} />
-      </div>
+    <nav className="bg-primary fixed top-0 z-50 flex h-[70px] w-full flex-row items-center justify-between p-5 sm:px-10 md:px-12 lg:px-24 xl:px-48">
+      <Link href="/" passHref>
+        <a className="fancy-ring fancy-ring-bg flex items-center justify-center rounded-md transition-colors hover:text-primary-500 focus:text-primary-500">
+          <LogoIcon size={24} />
+        </a>
+      </Link>
 
       <div>
         <ThemeSwitcher />
