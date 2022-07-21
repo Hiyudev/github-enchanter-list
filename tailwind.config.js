0c93c5ea7ec5a43e778f4ae0799ece0ed4f6a777
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-21 20:51:03 -0300

    feat: change application colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode: 'class',
   content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx'],
   theme: {
     extend: {
       colors: {
-        primary: colors.pink,
-        secondary: colors.purple,
+        primary: colors.emerald,
+        secondary: colors.blue,
         background: colors.zinc,
       },
     },
