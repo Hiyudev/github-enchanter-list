adc1d22e84b0880eb8c61acd72994c27d184b1f1
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-21 20:04:26 -0300

    fix: fix swr not fetching next pages

--- src/components/sections/Badge/BadgeList/index.tsx
+++ src/components/sections/Badge/BadgeList/index.tsx
@@ -1,12 +1,12 @@
+import { useMemo } from 'react';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import useSWRInfinite from 'swr/infinite';
 import { useBadge } from '../../../../lib/stores/badgeStore';
 import { useEditor } from '../../../../lib/stores/editorStore';
+import { urls } from '../../../../utils/url';
 import BadgeCard, { BadgeCardSkeleton } from './BadgeCard';
 
 function BadgeList() {
-  const style = useBadge((state) => state.style);
-  const search = useEditor((state) => state.search);
   const itemsQuantity = 36;
 
   function BadgeCardSkeletonList() {
@@ -20,22 +20,44 @@ function BadgeList() {
       </ul>
     );
   }
+  const style = useBadge((state) => state.style);
+  const search = useEditor((state) => state.search);
 
-  const getIcons = (pageIndex, previousPageData) => {
-    if (previousPageData && !previousPageData.length) return null;
-    return `/api/badges?page=${pageIndex}&limit=${itemsQuantity}&style=${style}&search=${search}`;
+  const getKey = (pageIndex, previousPageData) => {
+    if (previousPageData && !previousPageData.badges.length) return null;
+
+    const apiUrl = urls(
+      `/api/badges?page=${pageIndex}&limit=${itemsQuantity}&style=${style}`,
+      {
+        [`search=${search}`]: search.length > 0,
+      }
+    );
+    return apiUrl;
   };
 
   const fetcher = (url) => fetch(url).then((res) => res.json());
-  const { data, size, setSize } = useSWRInfinite(getIcons, fetcher);
+  const { data, size, setSize } = useSWRInfinite(getKey, fetcher);
+
+  const currentBadgeLength = useMemo(() => {
+    if (!data) return 0;
+    return data.map((res) => res.badges.length).reduce((a, b) => a + b, 0);
+  }, [data]);
+
+  const maxBadgeLength = useMemo(() => {
+    if (!data) return 0;
+    return data[0].total;
+  }, [data]);
 
   if (!data) return <BadgeCardSkeletonList />;
 
   return (
     <InfiniteScroll
-      dataLength={data.length}
-      next={() => setSize(size + 1)}
-      hasMore={data[0].total > size * itemsQuantity}
+      className="mb-8"
+      dataLength={currentBadgeLength}
+      next={() => {
+        setSize(size + 1);
+      }}
+      hasMore={maxBadgeLength > currentBadgeLength}
       loader={<BadgeCardSkeletonList />}
     >
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
