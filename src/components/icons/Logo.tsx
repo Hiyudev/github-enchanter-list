795a6b2ee9f7ee4b364b392c1ad55712d168ca87
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-21 13:11:42 -0300

    feat: add a11y tags on svg

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -11,8 +11,10 @@ const LogoIcon = (props: ILogoIconProps) => (
     viewBox="0 0 512 512"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    aria-labelledby="Github enchanter list logo"
     {...props}
   >
+    <title>Github enchanter list logo</title>
     <path
       d="M256 0L85 170.667L256 341.333L213.25 384L85 256V341.333V512H256H427L341.5 426.667H427V384L256 170.667L427 256V170.667L256 0Z"
       fill="currentColor"
