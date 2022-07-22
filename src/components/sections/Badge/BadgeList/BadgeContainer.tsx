abb97424b026c3b841462629db518822893bbd70
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-22 09:27:11 -0300

    feat: add className customization prop

--- src/components/sections/Badge/BadgeList/BadgeContainer.tsx
+++ src/components/sections/Badge/BadgeList/BadgeContainer.tsx
@@ -3,13 +3,18 @@ import { BadgeStyles } from '../../../../@types';
 
 type BadgeContainerProps = {
   children: React.ReactNode;
+  className?: string;
   style: BadgeStyles;
 };
 
-function BadgeContainer({ style, children }: BadgeContainerProps) {
+function BadgeContainer({
+  className = '',
+  style,
+  children,
+}: BadgeContainerProps) {
   return (
     <div
-      className={classNames(`relative min-w-[200px]`, {
+      className={classNames(`relative min-w-[200px] ${className}`, {
         'h-[18px]': style == 'plastic',
         'h-[20px]':
           style == 'flat' || style === 'flat-square' || style == 'social',
