a825391179c78f04b17ee72d0d67ff024b8ce7ed
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-22 20:58:13 -0300

    feat: make Options component accept div props

--- src/components/common/Options/index.tsx
+++ src/components/common/Options/index.tsx
@@ -1,13 +1,13 @@
-import { useEffect, useState } from 'react';
+import { ComponentProps, useEffect, useState } from 'react';
 import { Option } from '../../../@types';
 import OptionComponent from './Option';
 
-type OptionsProps = {
+type OptionsProps = ComponentProps<'div'> & {
   options: Option[];
   onSelectOption: (selected: Option) => void;
 };
 
-export default function Options({ options, onSelectOption }: OptionsProps) {
+export default function Options({ options, onSelectOption, ...props }: OptionsProps) {
   const [selected, selectOption] = useState<Option>(
     () => options.filter((option) => option.defaultSelected)[0]
   );
@@ -21,6 +21,7 @@ export default function Options({ options, onSelectOption }: OptionsProps) {
       role="toolbar"
       aria-label="Switch badge style options"
       className="flex flex-col md:flex-row"
+      {...props}
     >
       {options.map((option, index) => {
         function handleSelectOption() {
