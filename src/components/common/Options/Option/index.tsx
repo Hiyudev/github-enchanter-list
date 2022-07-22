f606603618bbd97e3e97e5d9b53e0022ba4f81a8
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-22 09:57:01 -0300

    fix: decrease opacity when option is not selected

--- src/components/common/Options/Option/index.tsx
+++ src/components/common/Options/Option/index.tsx
@@ -21,7 +21,7 @@ function OptionComponent({
         `flex w-full flex-col items-center justify-center p-4 transition-opacity hover:opacity-100 focus:opacity-100 ${className}`,
         {
           'bg-primary-500 opacity-100': selected,
-          'bg-secondary opacity-75': !selected,
+          'bg-secondary opacity-50': !selected,
           'rounded-t-md md:rounded-l-md md:rounded-tr-none': first,
           'rounded-b-md md:rounded-r-md md:rounded-bl-none': last,
         }
