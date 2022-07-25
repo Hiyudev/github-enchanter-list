74c5e75ad1109fa6dfdf96cc8168f85fd405f5b7
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-25 13:17:57 -0300

    feat: adapt to the badge generator

--- src/pages/api/badges.ts
+++ src/pages/api/badges.ts
@@ -2,10 +2,8 @@ import fuzzysort from 'fuzzysort';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { SimpleIcon } from 'simple-icons';
 import { Badge } from '../../@types';
-import { getConstratColor } from '../../lib/color';
 import { getIcons } from '../../lib/simpleIcons';
-import { urlEncode } from '../../utils/string';
-import { urls } from '../../utils/url';
+import { generateBadgeURL } from '../../utils/badge';
 
 type Data = {
   badges: Badge[];
@@ -28,6 +26,8 @@ const getBadgesHandler = (
   try {
     const queryPage = Number(page);
     const queryLimit = Number(limit);
+    const queryLabel = typeof label == 'string' ? label : label?.join(' ');
+    const queryStyle = typeof style == 'string' ? style : style?.join(' ');
 
     if (!isNaN(queryPage) && !isNaN(queryLimit)) {
       let iconsList: SimpleIcon[] = allIcons;
@@ -41,18 +41,12 @@ const getBadgesHandler = (
       const requestedIcons = iconsList.slice(start, end);
 
       requestedIcons.forEach((icon) => {
-        const logoColor = getConstratColor(icon.hex);
-        const labelString = label ? `${urlEncode(label as string)}-` : "";
-        const titleString = urlEncode(icon.title);
-
-        const hasStyle = typeof style === "string" && style.length > 0;
-
-        const templateUrl = `https://img.shields.io/badge/${labelString}${titleString}-${icon.hex}`
-
-        const url: string = urls(templateUrl, {
-          [`style=${style}`]: hasStyle,
-          [`logo=${icon.slug}`]: true,
-          [`logoColor=${logoColor}`]: style != 'social',
+        const url = generateBadgeURL({
+          hex: icon.hex,
+          title: icon.title,
+          label: queryLabel,
+          style: queryStyle,
+          slug: icon.slug
         });
 
         badges.push({
