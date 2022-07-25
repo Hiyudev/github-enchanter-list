e361c969ca596153fa86c6f5262fb62f7c207cdd
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-25 12:42:16 -0300

    fix: fix color contrast function

--- src/lib/color.ts
+++ src/lib/color.ts
@@ -1,6 +1,24 @@
-import constrast from 'contrast';
+function hexToRgb(hex) {
+  const hexColorRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i
+  const result = hexColorRegex.exec(hex)
+  return {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+  }
+}
+
+function brightness({ r, g, b }) {
+  return +((r * 299 + g * 587 + b * 114) / 255000).toFixed(2)
+}
 
 export function getConstratColor(hex: string): string {
-  const res = constrast(hex);
-  return res === 'light' ? 'black' : 'white';
+  const brightnessThreshold = 0.69
+  const rgb = hexToRgb(hex);
+
+  if (brightness(rgb) <= brightnessThreshold) {
+    return 'white';
+  } else {
+    return 'black';
+  }
 }
