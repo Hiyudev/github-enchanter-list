71a61cc0e72b3cabf02bdef7451b4b4b4078a7b8
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-27 12:00:41 -0300

    feat: add slider component

---
+++ src/components/common/Slider/index.tsx
@@ -0,0 +1,28 @@
+import * as RadixSlider from '@radix-ui/react-slider';
+
+type SliderProps = {
+  min: number;
+  max: number;
+  defaultValue: number;
+  onValueChange: (value: number) => void;
+};
+
+function Slider({ min, max, defaultValue, onValueChange }: SliderProps) {
+  return (
+    <RadixSlider.Root
+      onValueChange={(value) => onValueChange(value[0])}
+      className="relative flex h-8 w-full items-center"
+      defaultValue={[defaultValue]}
+      max={max}
+      min={min}
+      step={1}
+    >
+      <RadixSlider.Track className="bg-secondary relative h-[3px] flex-1">
+        <RadixSlider.Range className="absolute h-full bg-white" />
+      </RadixSlider.Track>
+      <RadixSlider.Thumb className="block h-4 w-4 rounded-full bg-white" />
+    </RadixSlider.Root>
+  );
+}
+
+export default Slider;
