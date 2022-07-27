8d857a73f9f693a6b62b91b52317bc021c29817e
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-07-27 12:08:21 -0300

    feat: add styles on hover and focus

--- src/components/common/Switch/index.tsx
+++ src/components/common/Switch/index.tsx
@@ -8,7 +8,7 @@ function Switch({ handleCheckChange, ...props }: SwitchProps) {
   return (
     <RadixSwitch.Root
       onCheckedChange={handleCheckChange}
-      className="bg-secondary border-secondary relative h-8 w-16 shrink-0 rounded-full border radix-state-checked:bg-primary-500"
+      className="bg-secondary border-secondary relative h-8 w-16 shrink-0 rounded-full border opacity-75 transition-opacity hover:opacity-100 focus:opacity-100 radix-state-checked:bg-primary-500 radix-state-checked:opacity-100"
       {...props}
     >
       <RadixSwitch.Thumb className="block h-4 w-4 translate-x-2 rounded-full bg-black transition-transform radix-state-checked:translate-x-10 dark:bg-white" />
